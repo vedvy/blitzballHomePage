6e06a2f89924eb7be3b4ba5e874561524274be20
vedvy <vedant.vyas@stonybrook.edu>
2025-05-28 20:06:53 -0400

    basic menu created

--- my-app/src/components/main_program.tsx
+++ my-app/src/components/main_program.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import BlitzBallMenu from "./menu";
 
 export default function BlitzBallHomePage()
 {
@@ -13,4 +14,20 @@ export default function BlitzBallHomePage()
     const set_view_to_eboard = () => {
         set_current_view("eboard");
     }
+
+    const set_view_to_baserun = () => 
+    {
+        set_current_view("baserun");
+    }
+
+    const set_view_to_home = () => 
+    {
+        set_current_view("home");
+    }
+
+    return (
+        <div className="main_site">
+            <BlitzBallMenu/>
+        </div>
+    );
 }
--- my-app/src/components/menu.tsx
+++ my-app/src/components/menu.tsx
@@ -2,5 +2,12 @@ import React from "react";
 
 export default function BlitzBallMenu()
 {
-    
+    return (
+        <div className="menuBar">
+            <span className="menuBarItems">Home</span>
+            <span className="menuBarItems">Our EBoard</span>
+            <span className="menuBarItems">Blitzball</span>
+            <span className="menuBarItems">Base Running</span>
+        </div>
+    )
 }
