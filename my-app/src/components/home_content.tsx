947bd2d9b490b581e9092a4c30739b0a68c42724
vedvy <vedant.vyas@stonybrook.edu>
2025-06-05 19:47:39 -0400

    created rough draft of main homepage for site, tomorrow will focus on Blitzball and Base running rules

--- my-app/src/components/home_content.tsx
+++ my-app/src/components/home_content.tsx
@@ -1,14 +1,43 @@
 import React from "react";
+import blitzballlogo from "./otherblitzballimage.jpg";
+import instagramlogo from "../images/InstaLogo.jpg";
+import groupmelogo from "../images/groupmelogo.jpeg"
 
 export default function MainHomePage()
 {
     return (
         <div className="homePage">
             <div className="homeTitle">
-                <img src="blitball.jpeg" alt="BlitzBall not loading"/>
+                <img src={blitzballlogo} alt="BlitzBall not loading" height={50} width={60} style={{float: "left", marginLeft: "30px"}}/>
+                <img src={blitzballlogo} alt="BlitzBall not loading" height={50} width={60} style={{float: "right", marginRight: "30px"}}/>
                 <h1>Welcome to Stony Brook's Official Blitzball Club!</h1>
+                
+            </div>
+
+            <div className="homeMainContent">
+                <div className="aboutContent">
+                    <h2>What is Blitzball?</h2>
+                    <p>Figure out how to explain it later. blah blah blah</p>
+                    <br/>
+
+                </div>
+
+                <div className="socialsContent">
+                    <h2>Follow us on Instagram!</h2>
+                    <p>This way you can stay up to date on the latest news for Stony Brook's Blitzball Club!</p>
+                    <p>Check out the GroupMe in the Insta bio to collaborate and talk with other Blitzball members!</p>
+                    <a href="https://www.instagram.com/" target="blank"><img src={instagramlogo} width={150} height={100} /></a>
+                    <a href="https://www.groupme.com/" target="blank"><img src={groupmelogo} width={150} height={100} style={{marginLeft: "70px"}}/></a>
+                </div>
             </div>
             
+            <div className="siteContent">
+                <h2>Click on the menu buttons above to view either our current eBoard or the rules for Blitzball and Base Running!</h2>
+            </div>
+
+            <div className="footer">
+                <p>Stony Brook BlitzBall Club, Last Updated: 2025</p>
+            </div>
         </div>
     )
 }
