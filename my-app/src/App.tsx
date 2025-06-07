0193787c34a7259601bcc0bcbb3b5cf7c0671806
vedvy <vedant.vyas@stonybrook.edu>
2025-06-06 23:20:14 -0400

    finished homepage  for now, taking a break from this to worj on actual project. Maybe i'll come back to this later?

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import BlitzBallHomePage from './components/main_program';
 import './App.css';
 
+document.title = "SBU Blitzball";
+
 function App() {
   return (
     <BlitzBallHomePage/>
--- my-app/src/components/home_content.tsx
+++ my-app/src/components/home_content.tsx
@@ -6,7 +6,7 @@ import groupmelogo from "../images/groupmelogo.jpeg"
 export default function MainHomePage()
 {
     return (
-        <div className="homePage">
+        <div className={"homePage"}>
             <div className="homeTitle">
                 <img src={blitzballlogo} alt="BlitzBall not loading" height={50} width={60} style={{float: "left", marginLeft: "30px"}}/>
                 <img src={blitzballlogo} alt="BlitzBall not loading" height={50} width={60} style={{float: "right", marginRight: "30px"}}/>
@@ -24,15 +24,20 @@ export default function MainHomePage()
 
                 <div className="socialsContent">
                     <h2>Follow us on Instagram!</h2>
+                    <a href="https://stonybrook.campuslabs.com/engage/organization/blitzballclub" target="blank">Click this link to join the club on SBEngaged!</a>
                     <p>This way you can stay up to date on the latest news for Stony Brook's Blitzball Club!</p>
                     <p>Check out the GroupMe in the Insta bio to collaborate and talk with other Blitzball members!</p>
                     <a href="https://www.instagram.com/" target="blank"><img src={instagramlogo} width={150} height={100} /></a>
                     <a href="https://www.groupme.com/" target="blank"><img src={groupmelogo} width={150} height={100} style={{marginLeft: "70px"}}/></a>
+                    
                 </div>
             </div>
             
             <div className="siteContent">
                 <h2>Click on the menu buttons above to view either our current eBoard or the rules for Blitzball and Base Running!</h2>
+                <br/>
+                <h2>Current Updates: </h2>
+                <p>No Updates for now, enjoy your summer!</p>
             </div>
 
             <div className="footer">
